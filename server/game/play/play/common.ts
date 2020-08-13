b0d1d44363a853998e4fd2b8a0a1f73a0d1e453d
Mikhail Nekrytov <mihail.nekrytov@gmail.com>
2020-08-13 16:40:12 +0300

    rework common battle function, make it more flat and understandable

--- server/game/play/play/common.ts
+++ server/game/play/play/common.ts
@@ -12,8 +12,9 @@ import Clearing from '../../../../model/board/Clearing';
 import { Card } from '../../../../model/Card';
 import { Cancel, NoPiecesOfFaction } from './rejections';
 import cardEffect from './cardEffect';
+import PlayablePlayer from '../../../../model/PlayablePlayer';
 
-export async function * birdsong (this: Client, faction: Faction): AsyncIterableIterator<void> {
+export async function * birdsong(this: Client, faction: Faction): AsyncIterableIterator<void> {
   // TODO: check victory conditions
   if (this.game.factions.includes(Faction.riverfolk) && faction !== Faction.riverfolk) {
     // TODO: purchase services
@@ -22,12 +23,12 @@ export async function * birdsong (this: Client, faction: Faction): AsyncIterable
   this.game.nextPhase();
 }
 
-export async function * daylight (this: Client, faction: Faction): AsyncIterableIterator<void> {
+export async function * daylight(this: Client, faction: Faction): AsyncIterableIterator<void> {
   // TODO: activate crafted effects
   this.game.nextPhase();
 }
 
-export async function * evening (this: Client, faction: Faction): AsyncIterableIterator<void> {
+export async function * evening(this: Client, faction: Faction): AsyncIterableIterator<void> {
   // TODO: activate crafted effects
   this.game.nextPhase();
 }
@@ -44,11 +45,12 @@ class CannotAffordCrafting extends Rejection {
 }
 
 const CANCEL = Symbol();
-export async function * craft (this: Client, resources: Suit[], faction_: Faction): AsyncIterableIterator<void> {
-  if (faction_ === Faction.marquise_bot) { return; }
+
+export async function * craft(this: Client, resources: Suit[], faction_: Faction): AsyncIterableIterator<void> {
+  // if (faction_ === Faction.marquise_bot) { return; }
   const faction = faction_;
 
-  async function * craftCard (this: Client, { card: index }: { card: number }, threadId: string) {
+  async function * craftCard(this: Client, { card: index }: { card: number }, threadId: string) {
     const card = this.game.factionData[faction]!.hand[index];
     if (!card || !card.cost || !subset(card.cost, resources)) {
       throw new CannotAffordCrafting(threadId, card);
@@ -59,7 +61,7 @@ export async function * craft (this: Client, resources: Suit[], faction_: Factio
     }
   }
 
-  async function * cancel (this: Client) {
+  async function * cancel(this: Client) {
     return CANCEL;
   }
 
@@ -83,14 +85,13 @@ function strength(game: Game, clearing: Clearing, faction: Faction): number {
     units += [...game.factionData[faction]!.items.refreshed, ...game.factionData[faction]!.items.exhausted]
       .filter(item => item.name === Item.sword)
       .length;
+
     const allies = Object.entries(game.factionData[faction]!.relations)
       .filter(([, level]) => level === 3)
       .map(([faction]) => faction);
+
     units += clearing.pieces
-      .filter(
-        piece => piece.name === 'warrior'
-              && allies.includes(piece.faction!)
-      )
+      .filter(piece => piece.name === 'warrior' && allies.includes(piece.faction!))
       .length;
   } else {
     units += clearing.pieces.filter(piece => Piece.equals(piece, Pieces[faction].warrior)).length;
@@ -99,7 +100,7 @@ function strength(game: Game, clearing: Clearing, faction: Faction): number {
   return units;
 }
 
-async function * takeHits (this: Client, hits: number, clearing: Clearing, faction: Faction): AsyncIterableIterator<void> {
+async function * takeHits(this: Client, hits: number, clearing: Clearing, faction: Faction): AsyncIterableIterator<void> {
   TAKING_HITS: for (let i = 0; i < hits; ++i) {
     if (faction === Faction.vagabond || faction === Faction.vagabond2) {
       // TODO: this is weird one
@@ -121,18 +122,20 @@ async function * takeHits (this: Client, hits: number, clearing: Clearing, facti
   }
 }
 
-export async function * battle (this: Client, clearing: Clearing, attacker: Faction) {
+export async function * battle(this: Client, clearing: Clearing, attacker: Faction) {
   const factions = new Set(
     clearing.pieces
       .map(piece => piece.faction)
-      .filter((faction): faction is Faction => faction !== null)
+      .filter((faction): faction is Faction => faction !== null),
   );
+  // whe we delete marquise here?
   factions.delete(Faction.marquise);
-  let defender = [...factions][0];
-  if (factions.size > 1) {
+
+  let defender: Faction = [...factions][0];
+  if (factions.size <= 1) {
     defender = yield * accept.call(this,
       cancel,
-      async function * faction (this: Client, { faction }: { faction: Faction }, threadId: string) {
+      async function * faction(this: Client, { faction }: { faction: Faction }, threadId: string) {
         if (factions.has(faction)) {
           return faction;
         }
@@ -141,49 +144,70 @@ export async function * battle (this: Client, clearing: Clearing, attacker: Fact
     );
   }
 
+  // Just a closure for ambush to avoid wall of nested if-s
+  const defenderPlayer: PlayablePlayer | undefined = this.game.factionData[defender];
+  const attackerPlayer: PlayablePlayer | undefined = this.game.factionData[attacker];
+  if (!defenderPlayer || !attackerPlayer) { return; }
+
   do {
-    if (defender !== Faction.marquise_bot && attacker !== Faction.marquise_bot) {
-      if (this.game.factionData[defender]!.hand.some(card => card.name === Card.ambush && (card.suit === clearing.suit || card.suit === Suit.bird))) {
-        const { card: index } = await this.game.sendTo(defender, 'ambush');
-        if (index !== undefined) {
-          const card = this.game.factionData[defender]!.hand[index];
-          if (card.name === Card.ambush && (card.suit === clearing.suit || card.suit === Suit.bird)) {
-            this.game.discard(...this.game.factionData[defender]!.hand.splice(index, 1));
-            if (this.game.factionData[attacker]!.hand.some(card => card.name === Card.ambush && (card.suit === clearing.suit || card.suit === Suit.bird))) {
-              const { card: index } = await this.game.sendTo(attacker, 'ambush');
-              if (index !== undefined) {
-                const card = this.game.factionData[attacker]!.hand[index];
-                if (card.name === Card.ambush && (card.suit === clearing.suit || card.suit === Suit.bird)) {
-                  this.game.discard(...this.game.factionData[attacker]!.hand.splice(index, 1));
-                  break;
-                }
-              }
-            }
-            yield * takeHits.call(this, 2, clearing, attacker);
-            if (strength(this.game, clearing, attacker) === 0) {
-              // battle ends if there are no more warriors left
-              return;
-            }
-          }
-        }
+    const isAmbushSuitable = (card: Card, clearing: Clearing): boolean => {
+      return card.name === Card.ambush && (card.suit === clearing.suit || card.suit === Suit.bird);
+    };
+    const hasAmbushCards = (player: PlayablePlayer, clearing: Clearing): boolean => {
+      return player!.hand.some((card: Card) => isAmbushSuitable(card, clearing));
+    };
+
+    // if there are no ambush cards in defender's hand, go further
+    if (!hasAmbushCards(defenderPlayer, clearing)) { break; }
+
+    // if player doesn't want to use ambush card, go further
+    const { card: defenderIndex } = await this.game.sendTo(defender, 'ambush');
+    if (defenderIndex === undefined) { break; }
+
+    // if card is incompatible, go further
+    const defenderCard = defenderPlayer!.hand[defenderIndex];
+    if (!isAmbushSuitable(defenderCard, clearing)) { break; }
+
+    // use defender's ambush card
+    this.game.discard(...defenderPlayer!.hand.splice(defenderIndex, 1));
+
+    // mini closure for attacker's ambush card
+    let discardAmbush: boolean = false;
+    do {
+      if (!hasAmbushCards(attackerPlayer, clearing)) { break; }
+
+      const { card: attackerIndex } = await this.game.sendTo(attacker, 'ambush');
+      if (attackerIndex === undefined) { break; }
+
+      const attackerCard = attackerPlayer!.hand[attackerIndex];
+
+      if (isAmbushSuitable(attackerCard, clearing)) {
+        this.game.discard(...attackerPlayer!.hand.splice(attackerIndex, 1));
+        discardAmbush = true;
       }
-    }
+    } while (false);
+
+    if (discardAmbush) { break; }
+
+    yield * takeHits.call(this, 2, clearing, attacker);
+
+    // battle ends if there are no more warriors left
+    if (strength(this.game, clearing, attacker) === 0) { return; }
   } while (false);
 
   let [attack, defend] = this.game.rollDice();
   if (defender === Faction.alliance) {
     [attack, defend] = [defend, attack];
   }
-  const [attackerStrength, defenderStrength] = [
-    strength(this.game, clearing, attacker),
-    strength(this.game, clearing, defender),
-  ];
-  [attack, defend] = [
-    Math.min(attack, attackerStrength),
-    Math.min(defend, defenderStrength),
-  ];
+
+  const defenderStrength = strength(this.game, clearing, defender);
+  const attackerStrength = strength(this.game, clearing, attacker);
+
+  attack = Math.min(attack, attackerStrength);
+  defend = Math.min(defend, defenderStrength);
+
   // bonus attacks
-  if (attacker !== Faction.marquise_bot && this.game.factionData[attacker]!.craftedEffects.some(card => card.name === Card.brutal_tactics)) {
+  if (attackerPlayer!.craftedEffects.some(card => card.name === Card.brutal_tactics)) {
     if (await this.game.sendTo(attacker, 'brutalTactics')) {
       attack++;
       this.game.factionData[defender]!.victoryPoints++;
@@ -193,16 +217,12 @@ export async function * battle (this: Client, clearing: Clearing, attacker: Fact
   if (attacker === Faction.eyrie && this.game.factionData.eyrie!.leader === Leader.commander) {
     attack++;
   }
-  if (defender === Faction.eyrie && this.game.factionData.eyrie!.leader === Leader.commander) {
-    defend++;
-  }
-  if (defenderStrength === 0) {
-    attack++;
-  }
+  if (defenderStrength === 0) { attack++; }
+
   yield * takeHits.call(this, defend, clearing, attacker);
   yield * takeHits.call(this, attack, clearing, defender);
 }
 
-export async function * cancel (this: Client): AsyncIterableIterator<void> {
+export async function * cancel(this: Client): AsyncIterableIterator<void> {
   throw new Cancel;
 }
