35fde9efa1f18db3ced8a75a3b6ba83cc82fc240
Mikhail Nekrytov <mihail.nekrytov@gmail.com>
2020-08-13 16:54:25 +0300

    format files and add come logs

--- client/game/play/common.ts
+++ client/game/play/common.ts
@@ -2,19 +2,24 @@ import { get } from 'svelte/store';
 import { game } from '../../store';
 import Client from '../../../model/Client';
 import Faction from '../../../model/Faction';
+import log from '../../util/log';
 
-export async function * birdsong (this: Client, faction: Faction): AsyncIterableIterator<void> {
+export async function * birdsong(this: Client, faction: Faction): AsyncIterableIterator<void> {
+  log(`Start ${faction} birdsong`);
   // TODO: check victory conditions
+
   if (get(game)!.factions.includes(Faction.riverfolk) && faction !== Faction.riverfolk) {
     // TODO: purchase services
   }
   // TODO: activate crafted effects
 }
 
-export async function * daylight (this: Client, faction: Faction): AsyncIterableIterator<void> {
+export async function * daylight(this: Client, faction: Faction): AsyncIterableIterator<void> {
+  log(`Start ${faction} daylight`);
   // TODO: activate crafted effects
 }
 
-export async function * evening (this: Client, faction: Faction): AsyncIterableIterator<void> {
+export async function * evening(this: Client, faction: Faction): AsyncIterableIterator<void> {
+  log(`Start ${faction} evening`);
   // TODO: activate crafted effects
 }
--- client/game/play/index.ts
+++ client/game/play/index.ts
@@ -6,22 +6,29 @@ import update from '../update';
 import marquiseTurn from './marquise';
 import Client from '../../../model/Client';
 import Faction from '../../../model/Faction';
+import log from '../../util/log';
 
-async function * turn (this: Client, faction: Faction): AsyncIterableIterator<void> {
+async function * turn(this: Client, faction: Faction): AsyncIterableIterator<void> {
   switch (faction) {
     case Faction.marquise:
+      log('Start marquise turn');
       yield * marquiseTurn.call(this);
       break;
     case Faction.eyrie:
+      log('Start eyrie turn');
       break;
     case Faction.alliance:
+      log('Start alliance turn');
       break;
     case Faction.vagabond:
     case Faction.vagabond2:
+      log('Start vagabond turn');
       break;
     case Faction.riverfolk:
+      log('Start riverfolk turn');
       break;
     case Faction.cult:
+      log('Start cult turn');
       break;
     default:
       throw new Error('unimplemented');
@@ -29,8 +36,8 @@ async function * turn (this: Client, faction: Faction): AsyncIterableIterator<vo
   yield * accept.call(this, update);
 }
 
-export default async function * play (this: Client) {
-  for (;;) {
+export default async function * play(this: Client) {
+  for (; ;) {
     const currentPlayer = borrow(game)(game => game!.playerNames[game!.turn! % game!.playerNames.length]);
     if (get(username) === currentPlayer) {
       turn.call(this, get(game)!.players[currentPlayer].faction!);
